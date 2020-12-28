35337375918e523bfea46d1d7a0ea1f072029e83
Shundeev Egor <shundeevegor@gmail.com>
2020-12-28 04:28:09 +0300

    Refactor out on-click handler

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,59 +1,63 @@
-var elements = document.querySelectorAll('.que');
+const getAnswer = (event) => {
+    const question = event.path.find(el => el.classList.contains('que'));
 
-var moodleSession = document
+    const qidString = question
+        .querySelector('.questionflagpostdata')
+        .value
+        ?.split('&')
+        .find(id => id.startsWith('qid='))
+        ?.split('=')[1];
+
+    const qid = parseInt(qidString);
+    browser.runtime.sendMessage({
+            type: 'answerClaim',
+            qid
+        })
+        .then((response) => {
+            if (!question.querySelector('.answer-found')) {
+                const answerSection = document.createElement('div');
+                answerSection.setAttribute('class', 'content formulation answer-found');
+                if (response.status === 'CORRECT') {
+                    answerSection.setAttribute('style', 'text-align: center');
+                } else {
+                    answerSection.setAttribute('style', 'text-align: center;background-color: #f5ebe7');
+                }
+
+                const screenshot = document.createElement('img');
+                screenshot.setAttribute('class', 'screenshot');
+                screenshot.setAttribute('src', response.screenshot);
+                screenshot.setAttribute('width', 200);
+                screenshot.setAttribute('height', 200);
+
+                answerSection.append(screenshot);
+                question.append(answerSection);
+            } else {
+                const answerSection = el.querySelector('.answer-found');
+                if (response.status === 'CORRECT') {
+                    answerSection.setAttribute('style', 'text-align: center');
+                } else {
+                    answerSection.setAttribute('style', 'text-align: center;background-color: #f5ebe7');
+                }
+
+                const screenshot = answerSection.querySelector('.screenshot');
+                screenshot.setAttribute('src', response.screenshot);
+            }
+        });
+};
+
+const moodleSession = document
     .cookie
     .split('; ')
     .find((cookie) => cookie.startsWith('MoodleSession='))
     ?.split('=')[1];
 
+const elements = document.querySelectorAll('.que');
+
 if (moodleSession)
     for (let el of elements) {
-        var getAnswer = (event) => {
-            browser.runtime.sendMessage({type: 'answerClaim', qid})
-                .then((response) => {
-                    if (!el.querySelector('.answer-found')) {
-                        var answerSection = document.createElement('div');
-                        answerSection.setAttribute('class', 'content formulation answer-found');
-                        if (response.status === 'CORRECT') {
-                            answerSection.setAttribute('style', 'text-align: center');
-                        } else {
-                            answerSection.setAttribute('style', 'text-align: center;background-color: #f5ebe7');
-                        }
+        const formulation = el.querySelector('.formulation');
 
-                        var screenshot = document.createElement('img');
-                        screenshot.setAttribute('class', 'screenshot');
-                        screenshot.setAttribute('src', response.screenshot);
-                        screenshot.setAttribute('width', 200);
-                        screenshot.setAttribute('height', 200);
-
-                        answerSection.append(screenshot);
-                        el.append(answerSection);
-                    } else {
-                        var answerSection  =  el.querySelector('.answer-found');
-                        if (response.status === 'CORRECT') {
-                            answerSection.setAttribute('style', 'text-align: center');
-                        } else {
-                            answerSection.setAttribute('style', 'text-align: center;background-color: #f5ebe7');
-                        }
-
-                        var screenshot = answerSection.querySelector('.screenshot');
-                        screenshot.setAttribute('src', response.screenshot);
-                    }
-                });
-        };
-
-        var qidString = el
-            .querySelector('.questionflagpostdata')
-            .value
-            ?.split('&')
-            .find(id => id.startsWith('qid='))
-            ?.split('=')[1];
-
-        var qid = parseInt(qidString);
-
-        var formulation = el.querySelector('.formulation');
-
-        var answerButton = document.createElement('div');
+        const answerButton = document.createElement('div');
         answerButton.setAttribute('class', 'submit btn btn-secondary');
         answerButton.textContent = browser.i18n.getMessage('answerButton');
         answerButton.addEventListener('click', getAnswer);
